e37c5c7da2ae08bd94c1345c284ac269b6724346
Denise Pereira <deniseluvoir@gmail.com>
2022-04-20 21:30:17 -0300

    Init

--- src/Main.kt
+++ src/Main.kt
@@ -1,25 +1,19 @@
-
-class Pessoa (val anoNascimento: Int, var nome:String){
-    var olhos: String? = null
-    var doc: String =" "
-
-    constructor(anoNascimento: Int, nome: String, doc:String): this(anoNascimento, nome){
-        this.doc = doc
+class Animal( var especie: String){
+    var fala:String =""
+    init {
+       if (especie =="dog"){
+            fala ="au-au"
+        }else if(especie == "gato"){
+            fala = "miauu"
+        }else{
+            ""
+       }
     }
-    fun dormir(){
-
-    }
-    fun acordar(){
-
+    fun falar(){
+        println(fala)
     }
 }
-
 fun main(){
-    var pessoa: Pessoa = Pessoa(2005, "Denise", "123456789")
-    pessoa.nome
-    pessoa.dormir()
-    pessoa.acordar()
-    pessoa.olhos
-    pessoa.doc
+     Animal("dog").falar()
 
 }
